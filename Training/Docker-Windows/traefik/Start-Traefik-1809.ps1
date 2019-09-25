8f55b2937329d682e8ce383e6c90041f897318cc
Chui Tey <teyc@cognoware.com>
2019-09-25 15:33:26 +1000

    Create Start-Traefik-1809.ps1

---
+++ Training/Docker-Windows/traefik/Start-Traefik-1809.ps1
@@ -0,0 +1,7 @@
+docker run -it -p 8080:8080 -p 80:80 `
+  --rm `
+  --name traefik `
+  -v $WPD/.:C:\etc\traefik
+  traefik:v1.7.17-windowsservercore-1809 `
+  --debug --docker.endpoint=npipe:////./pipe/docker_engine
+  
